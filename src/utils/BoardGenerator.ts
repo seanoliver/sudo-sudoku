458cb631f57a42e469eb92a34c258f7f3cff210a
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-01-19 13:19:16 -0800

    basic constructor for board generator

--- src/utils/BoardGenerator.ts
+++ src/utils/BoardGenerator.ts
@@ -2,4 +2,9 @@ import { Board } from "./SudokuSolver";
 
 export class BoardGenerator {
   private board: Board;
+  constructor() {
+    this.board = Array(9)
+      .fill(0)
+      .map(() => Array(9).fill(0));
+  }
 }
