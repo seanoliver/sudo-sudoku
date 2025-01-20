28a717e2500a93a2d8e08b66e32a8d22ca36466c
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-01-20 10:03:11 -0800

    add lodash, begin to flesh out board generator

--- src/utils/BoardGenerator.ts
+++ src/utils/BoardGenerator.ts
@@ -1,10 +1,19 @@
 import { Board } from "./SudokuSolver";
 
+
 export class BoardGenerator {
   private board: Board;
   constructor() {
     this.board = Array(9)
       .fill(0)
       .map(() => Array(9).fill(0));
+  }
+
+  private fillBox(row: number, col: number, num: number): boolean {
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        
+      }
+    }
   }
 }
